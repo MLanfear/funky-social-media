9dda853d5e84a54db46443cfe8238d10e7a26a5a
MLanfear <maxjlanfear@gmail.com>
2022-12-29 23:37:00 -0800

    updated and completed thought.js section

--- models/thought.js
+++ models/thought.js
@@ -1,7 +1,8 @@
 const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const ReactionSchema = new Schema (
-    {
+// CREATE REACTION SCHEMA
+const reactionSchema = new Schema ({
         reactionId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId()
@@ -10,53 +11,44 @@ const ReactionSchema = new Schema (
             type:String,
             required:true,
             maxLength: 280,
-            trim: true
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => {
-                let month = createdAtVal.getMonth() + 1;
-                let day = createdAtVal.getDate();
-                let year = createdAtVal.getFullYear();
-                return `${month}/${day}/${year}`;
-            }
         },
         username: {
             type:String,
-            required: 'You must include username in request'
+            required: true
         },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+},
+{
+    toJSON: {
+        getters: true
     },
-    {
-        toJSON: {
-            getters: true
-        },
+    id: false
 
-    }
-);
+});
 
-const ThoughtSchema = new Schema (
+const thoughtSchema = new Schema (
     {
         thoughtText: {
             type:String,
-            maxLength: 280,
-            trim: true
+            required: true,
+            minlength: 1,
+            maxlength: 280
         },
         createdAt: {
             type: Date,
             default: Date.now,
-            get: createdAtVal => {
-                let month = createdAtVal.getMonth() + 1;
-                let day = createdAtVal.getDate();
-                let year = createdAtVal.getFullYear();
-                return `${month}/${day}/${year}`;
-            }
+            get: createdAtVal => dateFormat(createdAtVal)
         },
         username: {
             type:String,
             required: 'You must include username in request'
         },
-        reactions:[ReactionSchema]
+        reactions:[reactionSchema]
     },
     {
         toJSON: {
@@ -67,9 +59,9 @@ const ThoughtSchema = new Schema (
     }
 );
 
-ThoughtSchema.virtual('reactionCount').get(function() {
+thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
 
-const Thought = model ('Thought', ThoughtSchema);
+const Thought = model ('Thought', thoughtSchema);
 module.exports = Thought;
